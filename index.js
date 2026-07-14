d125d4fbce434f26935df9524892ec3c628911a7
Julius-93 <julius.irungu@student.moringaschool.com>
2026-07-14 09:07:10 +0300

    Add combineUsers function to merge user arrays
    Implemented combineUsers function to merge user arrays and add merge date.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+require('datejs');
 
+function combineUsers(...args) {
+  // make the object with an empty users array
+  let combinedObject = {
+    users: []
+  };
 
+  // go through each array that got passed in
+  for (let i = 0; i < args.length; i++) {
+    // add this array's names into users
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
+
+  // get today's date
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+module.exports = combineUsers;
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
